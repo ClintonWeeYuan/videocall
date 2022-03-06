787520dabbebd4efdc60c7cf34ceaba95180c572
Clinton Wee <clinton_wee@hotmail.com>
2022-03-06 23:26:21 +0800

    Copy Link function added to the Room page

--- pages/[room].tsx
+++ pages/[room].tsx
@@ -1,15 +1,16 @@
 import { useRouter } from "next/router";
 import {NextPage} from "next";
-import {Box} from "@chakra-ui/react"
+import {Container, Box, Button} from "@chakra-ui/react"
 import Nav from '../components/Nav'
 import {User} from '../components/UserContext'
+
 
 
 const Room : NextPage = () => {
     const username = User();
     const router = useRouter();
     return(<div><Nav/>
-        <Box><h1>{router.query.room}</h1><h2>{username}</h2></Box></div>)
+        <Container><h1>Room Id: {router.query.room}</h1><h2>Username: {username}</h2><Button onClick={() => {navigator.clipboard.writeText(`http://localhost:3000/${router.query.room}`)}}>Copy Link</Button></Container></div>)
 }
 
 export default Room;
