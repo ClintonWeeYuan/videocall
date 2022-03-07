aef6e829e1256c734afa981c57f7fbabe2ff2700
Clinton Wee <clinton_wee@hotmail.com>
2022-03-07 23:59:04 +0800

    Added props definition for VideoRoom

--- components/VideoRoom.tsx
+++ components/VideoRoom.tsx
@@ -1,7 +1,9 @@
 import {NextPage} from "next";
 import {Box} from '@chakra-ui/react'
 
-
+type Props = {
+    username: string;
+}
 const VideoRoom: NextPage<Props> = ({username}) => {
     return (<Box>Hello {username}</Box>)
 }
