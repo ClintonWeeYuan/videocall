4317bfb76b0d7ebc409f2909a3b2848083dc82e7
ClintonWeeYuan <clinton_wee@hotmail.com>
2022-03-20 09:59:08 +0800

    Repaired scroll bug

--- components/VideoRoom.tsx
+++ components/VideoRoom.tsx
@@ -277,7 +277,7 @@ const VideoRoom: NextPage<Props> = ({username}) => {
   }
   
   //Scroll to Bottom of ChatBox
-  const messagesEndRef = useRef(null);
+  const messagesEndRef = useRef<null | HTMLDivElement>(null);
   
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({behavior: "smooth"})
